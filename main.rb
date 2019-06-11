4bb7767ae0774c1a3736d77ad3f42faa0fa576e0
keshav-c <keshav1985@gmail.com>
2019-06-11 16:49:11 +0530

    change indent size, %w delimiter

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 def bubble_sort(array)
   array.size.downto(1) do |len|
     1.upto(len - 1) do |idx|
-        array[idx - 1], array[idx] = array[idx], array[idx - 1] if array[idx - 1] > array[idx]
+      array[idx - 1], array[idx] = array[idx], array[idx - 1] if array[idx - 1] > array[idx]
     end
   end
   array
@@ -22,6 +22,6 @@ p bubble_sort([4, 3, 78, 2, 0, 2])
 # => [0, 2, 2, 3, 4, 78]
 
 # test on string array
-var = %w(hi hello hey a bull ball)
+var = %w[hi hello hey a bull ball]
 p bubble_sort_by(var) { |left, right| left.length - right.length }
 # => ["a", "hi", "hey", "bull", "ball", "hello"]
