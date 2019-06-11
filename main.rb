ee5e85b26937076bbcc22daa4e1a6950fcf1d06d
keshav-c <keshav1985@gmail.com>
2019-06-11 16:44:00 +0530

    refactor, prettify

--- main.rb
+++ main.rb
@@ -1,9 +1,7 @@
 def bubble_sort(array)
   array.size.downto(1) do |len|
     1.upto(len - 1) do |idx|
-      if array[idx - 1] > array[idx]
-        array[idx - 1], array[idx] = array[idx], array[idx - 1]
-      end
+        array[idx - 1], array[idx] = array[idx], array[idx - 1] if array[idx - 1] > array[idx]
     end
   end
   array
@@ -13,19 +11,17 @@ def bubble_sort_by(array)
   array.size.downto(1) do |len|
     1.upto(len - 1) do |idx|
       test = yield(array[idx - 1], array[idx])
-      if test > 0
-        array[idx - 1], array[idx] = array[idx], array[idx - 1]
-      end
+      array[idx - 1], array[idx] = array[idx], array[idx - 1] if test.positive?
     end
   end
   array
 end
 
 # test on integer array
-p bubble_sort([4,3,78,2,0,2])
-# => [0,2,2,3,4,78]
+p bubble_sort([4, 3, 78, 2, 0, 2])
+# => [0, 2, 2, 3, 4, 78]
 
 # test on string array
-var = ["hi","hello","hey"]
-p bubble_sort_by(var) {|left,right| left.length - right.length} 
-#=>["hi", "hey", "hello"]
+var = %w(hi hello hey a bull ball)
+p bubble_sort_by(var) { |left, right| left.length - right.length }
+# => ["a", "hi", "hey", "bull", "ball", "hello"]
